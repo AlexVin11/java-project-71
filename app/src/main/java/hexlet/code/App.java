761c479dcf3ae589197a40ff8034a58a2adac665
Alexander Vinogradow <alukard981@gmail.com>
2024-04-24 18:04:55 +0300

    modified to complete step 3- show info message

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,10 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.exit(new CommandLine(new Differ()).execute(args));
+        //System.out.println("Hello World!");
     }
 }
