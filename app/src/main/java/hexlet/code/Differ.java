47614412f4c4bf86ccba6c7d0c287963de8a9c9a
Alexander Vinogradow <alukard981@gmail.com>
2024-04-24 23:52:40 +0300

    Step 4 completed

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -9,12 +9,14 @@ import picocli.CommandLine.Parameters;
 
 public class Differ implements Runnable{
 
-    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]") String format;
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]", paramLabel = "format")
+    String format;
     @Option (names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
     boolean helpRequested = false;
-    @Option (names = {"-V", "--version"}, description = "Print version information and exit.") boolean version;
-    @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1") String filepath1;
-    @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2") String filepath2;
+    @Option (names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    boolean versionInfoRequested = false;
+    @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1") String pathToFirstFile;
+    @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2") String pathToSecondFile;
 
     /*public static void generate() {
         System.out.println("generate created");
