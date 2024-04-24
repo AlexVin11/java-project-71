3535ea080de7d4d088b1303a43fbda13009d546d
Alexander Vinogradow <alukard981@gmail.com>
2024-04-24 22:52:02 +0300

    String parameters added to be able to work with path to file1 and file 2. Option -f, --format to be able to choose format added.

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -2,15 +2,20 @@ package hexlet.code;
 
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
 
 @Command(name = "gendiff",
         description = "Compares two configuration files and shows a difference.")
 
 public class Differ implements Runnable{
 
+    @Option (names = {"-f", "--format"}, description = "output format [default: stylish]", paramLabel = "format")
+    String format;
     @Option (names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
     boolean help;
     @Option (names = {"-V", "--version"}, description = "Print version information and exit.") boolean V;
+    @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1") String filepath1;
+    @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2") String filepath2;
 
     /*public static void generate() {
         System.out.println("generate created");
