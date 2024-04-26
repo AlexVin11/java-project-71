56e7eaf57cf5c2302474072c43e03069bf03b126
Alexander Vinogradow <alukard981@gmail.com>
2024-04-26 16:19:48 +0300

    generate() now recieves a paths to files to calculate a difference between them (according to the requirements it should return a String)

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,29 +1,23 @@
 package hexlet.code;
 
-import picocli.CommandLine.Option;
-import picocli.CommandLine.Command;
-import picocli.CommandLine.Parameters;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-@Command(name = "gendiff",
-        description = "Compares two configuration files and shows a difference.")
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
 
-public class Differ implements Runnable{
 
-    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]", paramLabel = "format")
-    String format = "stylish";
-    @Option (names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
-    boolean helpRequested = false;
-    @Option (names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
-    boolean versionInfoRequested = false;
-    @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1") String pathToFirstFile;
-    @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2") String pathToSecondFile;
-
-    /*public static String generate() {
-        System.out.println("generate created");
-    }*/
-
-    @Override
-    public void run() {
-        System.out.println("");
+public class Differ{
+    public static String generate(Path p1, Path p2) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String firstFileContent = Files.readString(p1);
+        String secondFileContent = Files.readString(p2);
+        Map<String, Object> firstFileAsMap
+                = objectMapper.readValue(firstFileContent, new TypeReference<Map<String,Object>>(){});
+        Map<String, Object> secondFileAsMap
+                = objectMapper.readValue(secondFileContent, new TypeReference<Map<String,Object>>(){});
+        return "generate created";
     }
 }
