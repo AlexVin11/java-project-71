9f9ebb1dd35176eee64ac08550cb3c5f23b36005
Alexander Vinogradow <alukard981@gmail.com>
2024-05-04 15:06:23 +0300

    generete method was splitted to 4 methods. 1. generatePath method recieves string (user input (String)- path to file) conversts it in Path 2. readFileContent method recieves Path to file, read it and put content to the String 3. generateHashMapFromFileContent method recieves String with content of the file and parse it to the HashMap<String, Object> 4. generateKeyStatusHashMap method recieves 2 hashMap with first file and second file content and generates the status of the keys (was a value of this key edited or not, was key deleted or not) and puts result in SortedMap<String, String> (key=key edited/ keynotedited) 5. generate method recievs SortedMap, first file map and second file map and generates the String (message that will be shown to user)

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,11 +1,13 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class Differ{
@@ -13,53 +15,65 @@ public class Differ{
     static final String[] KEYSTATUS = {"key edited", "key not edited"};
     static final Character[] EDITSIGN = {'+', '-'};
 
-    public static String generate(Path p1, Path p2) throws IOException {
+    public static Path generatePath(String path) {
+        return Paths.get(path).toAbsolutePath().normalize();
+    }
+
+    public static String readFileContent(Path path) throws IOException {
+        return Files.readString(path);
+    }
+
+    public static HashMap<String, Object> generateHashMapFromFileContent(String fileContent) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
-        String firstFileContent = Files.readString(p1);
-        String secondFileContent = Files.readString(p2);
-        HashMap<String, Object> firstFileAsMap
-                = (HashMap<String, Object>) objectMapper.readValue(firstFileContent, new TypeReference<Map<String, Object>>() {
+        HashMap<String, Object> fileContentAsHashMap
+                = (HashMap<String, Object>) objectMapper.readValue(fileContent, new TypeReference<Map<String, Object>>() {
         });
-        var firstMapEntrys = firstFileAsMap.entrySet();
-        HashMap<String, Object> secondFileAsMap
-                = (HashMap<String, Object>) objectMapper.readValue(secondFileContent, new TypeReference<Map<String, Object>>() {
-        });
-        var secondMapEntrys = secondFileAsMap.entrySet();
+        return fileContentAsHashMap;
+    }
+
+    public static SortedMap<String, String> generateKeyStatusHashMap(HashMap<String, Object> firstFile,
+                                                                     HashMap<String, Object> secondFile) {
+        var firstMapEntrys = firstFile.entrySet();
+        var secondMapEntrys = secondFile.entrySet();
         SortedMap<String, String> mapOfKeysStatus = new TreeMap<>();
         for (var secondFileEntry : secondMapEntrys) {
-            if (firstFileAsMap.containsKey(secondFileEntry.getKey()) //key and value in both maps are equal - means just show it in result map
-                    && firstFileAsMap.get(secondFileEntry.getKey()).equals(secondFileEntry.getValue())) {
+            if (firstFile.containsKey(secondFileEntry.getKey()) //key and value in both maps are equal - means just show it in result map
+                    && firstFile.get(secondFileEntry.getKey()).equals(secondFileEntry.getValue())) {
                 mapOfKeysStatus.put(secondFileEntry.getKey(), KEYSTATUS[1]);
             }
-            if (!firstFileAsMap.containsKey(secondFileEntry.getKey())) { //key from second file absent in first file - means it was added
+            if (!firstFile.containsKey(secondFileEntry.getKey())) { //key from second file absent in first file - means it was added
                 mapOfKeysStatus.put(secondFileEntry.getKey(), KEYSTATUS[0]);
             }
-            if (firstFileAsMap.containsKey(secondFileEntry.getKey()) //key is present in both files but values are different - means it was edited
-                    && !firstFileAsMap.get(secondFileEntry.getKey()).equals(secondFileEntry.getValue())) {
+            if (firstFile.containsKey(secondFileEntry.getKey()) //key is present in both files but values are different - means it was edited
+                    && !firstFile.get(secondFileEntry.getKey()).equals(secondFileEntry.getValue())) {
                 mapOfKeysStatus.put(secondFileEntry.getKey(), KEYSTATUS[0]);
             }
         }
         for (var firstFileEntry : firstMapEntrys) {
-            if (!secondFileAsMap.containsKey(firstFileEntry.getKey())) { //key from first file is absent in second file - means it was deleted from first file
+            if (!secondFile.containsKey(firstFileEntry.getKey())) { //key from first file is absent in second file - means it was deleted from first file
                 mapOfKeysStatus.put(firstFileEntry.getKey(), KEYSTATUS[0]);
             }
         }
+        return mapOfKeysStatus;
+    }
+
+    public static String generate(SortedMap<String, String> mapOfKeyStatus,
+                                  HashMap<String, Object> firstMap, HashMap<String, Object> secondMap) {
         StringBuilder resultMessage = new StringBuilder("{");
-        for (String key : mapOfKeysStatus.keySet()) {
-            if (mapOfKeysStatus.get(key).equals(KEYSTATUS[1])) {
-                resultMessage.append("\n" + " ".repeat(4) + key + ": " + firstFileAsMap.get(key) + ", ");
+        for (String key : mapOfKeyStatus.keySet()) {
+            if (mapOfKeyStatus.get(key).equals(KEYSTATUS[1])) {
+                resultMessage.append("\n" + " ".repeat(4) + key + ": " + firstMap.get(key) + ", ");
             }
-            if (mapOfKeysStatus.get(key).equals(KEYSTATUS[0])) {
-                if (firstFileAsMap.containsKey(key)) {
-                    resultMessage.append("\n" + " ".repeat(2) + EDITSIGN[1] + " " + key + ": " + firstFileAsMap.get(key) + ", ");
+            if (mapOfKeyStatus.get(key).equals(KEYSTATUS[0])) {
+                if (firstMap.containsKey(key)) {
+                    resultMessage.append("\n" + " ".repeat(2) + EDITSIGN[1] + " " + key + ": " + firstMap.get(key) + ", ");
                 }
-                if (secondFileAsMap.containsKey(key)) {
-                    resultMessage.append("\n" + " ".repeat(2) + EDITSIGN[0] + " " + key + ": " + secondFileAsMap.get(key) + ", ");
+                if (secondMap.containsKey(key)) {
+                    resultMessage.append("\n" + " ".repeat(2) + EDITSIGN[0] + " " + key + ": " + secondMap.get(key) + ", ");
                 }
             }
         }
         resultMessage.delete(resultMessage.length() - 2, resultMessage.length()).append("\n" + "}");
-        String result = resultMessage.toString();
-        return result;
+        return resultMessage.toString();
     }
 }
