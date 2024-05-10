07fe8eadd4941a3d9cf18e2d49986c2810853c40
Alexander Vinogradow <alukard981@gmail.com>
2024-05-10 16:31:20 +0300

    first attempt to make test for comparing two flat json files with using a correct result json file

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -2,10 +2,26 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DifferTest {
 
-    @Test
-    public void testGenereatePath() {
+    static String CORRECTRESULTOFFILE1ANDFILE2COMPARE;
+    static {
+        try {
+            CORRECTRESULTOFFILE1ANDFILE2COMPARE = Files.readString(Path.of("test/resources/result.json"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    @Test
+    public void testGenerate() throws IOException {
+        assertEquals(CORRECTRESULTOFFILE1ANDFILE2COMPARE,
+                Differ.generate("test/resources/file1.json", "test/resources/file2.json"));
     }
 }
