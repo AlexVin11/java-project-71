f2e7fb6cbfd059e2ed5e148f315e05b4cb3cb722
Alexander Vinogradow <alukard981@gmail.com>
2024-05-11 15:46:00 +0300

    reference string is created to compare the output string of generate method

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -3,27 +3,23 @@ package hexlet.code;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.nio.file.Path;
 
-import static java.nio.file.Files.readString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DifferTest {
 
-    static Path pathToResultFile = Path.of("src/test/resources/result.json").toAbsolutePath().normalize();
-    static String resultFileContent;
-
-    static {
-        try {
-            resultFileContent = readString(pathToResultFile);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    static final String CORRECT_RESULT_OF_COMPARING_1 = "{\\n"
+            + "  - follow: false,\n"
+            + "    host: hexlet.io,\n"
+            + "  - proxy: 123.234.53.22,\n"
+            + "  - timeout: 50,\n"
+            + "  + timeout: 20,\n"
+            + "  + verbose: true\n"
+            + "}";
 
     @Test
     public void testGenerate() throws IOException {
-        assertEquals(resultFileContent,
+        assertEquals(CORRECT_RESULT_OF_COMPARING_1,
                 Differ.generate("src/test/resources/file1.json", "src/test/resources/file2.json"));
     }
 }
