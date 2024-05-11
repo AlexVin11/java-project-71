2fbaf2e588f29964196271a5ab161888b50b3f8e
Alexander Vinogradow <alukard981@gmail.com>
2024-05-11 16:46:06 +0300

    attempt to use txt file as reference

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -3,23 +3,27 @@ package hexlet.code;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.nio.file.Path;
 
+import static java.nio.file.Files.readString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DifferTest {
 
-    static final String CORRECT_RESULT_OF_COMPARING_1 = "{\n"
-            + "  - follow: false,\n"
-            + "    host: hexlet.io,\n"
-            + "  - proxy: 123.234.53.22,\n"
-            + "  - timeout: 50,\n"
-            + "  + timeout: 20,\n"
-            + "  + verbose: true\n"
-            + "}";
+    static Path pathToResultFile = Path.of("src/test/resources/result.txt").toAbsolutePath().normalize();
+    static String resultFileContent;
+
+    static {
+        try {
+            resultFileContent = readString(pathToResultFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Test
     public void testGenerate() throws IOException {
-        assertEquals(Differ.generate("src/test/resources/file1.json", "src/test/resources/file2.json"),
+        assertEquals(resultFileContent,
                 Differ.generate("src/test/resources/file1.json", "src/test/resources/file2.json"));
     }
 }
