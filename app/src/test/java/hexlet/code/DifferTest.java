915fb04c29296cc634370c437e05347770aceb62
Alexander Vinogradow <alukard981@gmail.com>
2024-05-13 15:47:36 +0300

    test for comparing 2 flat yaml files added

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -10,12 +10,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DifferTest {
 
-    static Path pathToResultFile = Path.of("src/test/resources/result.txt").toAbsolutePath().normalize();
-    static String resultFileContent;
+    static Path pathToResultForJsonFiles = Path.of("src/test/resources/resultjson.txt").toAbsolutePath().normalize();
+    static String jsonCompareFileContent;
 
     static {
         try {
-            resultFileContent = readString(pathToResultFile);
+            jsonCompareFileContent = readString(pathToResultForJsonFiles);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static Path pathToResultForYamlFiles = Path.of("src/test/resources/resultyaml.txt").toAbsolutePath().normalize();
+    static String yamlCompareFileContent;
+
+    static {
+        try {
+            yamlCompareFileContent = readString(pathToResultForYamlFiles);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -23,7 +34,9 @@ public class DifferTest {
 
     @Test
     public void testGenerate() throws IOException {
-        assertEquals(resultFileContent,
+        assertEquals(jsonCompareFileContent,
                 Differ.generate("src/test/resources/file1.json", "src/test/resources/file2.json"));
+        assertEquals(yamlCompareFileContent,
+                Differ.generate("src/test/resources/file1.yaml", "src/test/resources/file2.yaml"));
     }
 }
