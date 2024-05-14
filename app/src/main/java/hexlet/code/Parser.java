cd0b5ede2d4082641d1ef959f463d8c2b62132c0
Alexander Vinogradow <alukard981@gmail.com>
2024-05-14 16:50:01 +0300

    Parser class created with parser method. According to file content it determine the file format and parse content into hashmap

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -1,4 +1,22 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import java.util.HashMap;
+
 public class Parser {
+
+    public static HashMap<String, Object> parser(String content) throws Exception {
+        HashMap<String, Object> contentAsMap = new HashMap<>();
+        if (content.startsWith("---")) {
+            ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+            contentAsMap = mapper.readValue(content, HashMap.class);
+            return contentAsMap;
+        } else {
+            ObjectMapper mapper = new ObjectMapper();
+            contentAsMap = mapper.readValue(content, HashMap.class);
+            return contentAsMap;
+        }
+    }
 }
