a321af4c3678630cd0506f82080fcbfce4e3448a
Alexander Vinogradow <alukard981@gmail.com>
2024-05-17 22:42:13 +0300

    everything is moved back as it was. To compare two hashmaps with string object inside i need to use OBJECTS equals method to compare values. Only this method provides comparing 2 null values as regular values

--- app/src/main/java/hexlet/code/Comparator.java
+++ app/src/main/java/hexlet/code/Comparator.java
@@ -1,6 +1,7 @@
 package hexlet.code;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
@@ -9,40 +10,22 @@ import static hexlet.code.Differ.KEY_STATUS;
 
 public class Comparator {
 
-    public static SortedMap<String, String> generateKeyStatusHashMap(Map<String, String> firstFile,
-                                                                     Map<String, String> secondFile) {
+    public static SortedMap<String, String> generateKeyStatusHashMap(Map<String, Object> firstFile,
+                                                                     Map<String, Object> secondFile) {
         var firstMapEntrys = firstFile.entrySet();
         var secondMapEntrys = secondFile.entrySet();
         SortedMap<String, String> mapOfKeysStatus = new TreeMap<>();
 
         for (var secondFileEntry : secondMapEntrys) {
-            /*1*/if (firstFile.containsKey(secondFileEntry.getKey())
-                    && firstFile.get(secondFileEntry.getKey()).equals(secondFileEntry.getValue())) {
-                mapOfKeysStatus.put(secondFileEntry.getKey(), KEY_STATUS[1]);//key not edited (key and value is the same in both files)
-            }
-            /*3*/if (!firstFile.containsKey(secondFileEntry.getKey())) {
-                mapOfKeysStatus.put(secondFileEntry.getKey(), KEY_STATUS[2]);//key added (key from second file is absent in first file)
-            }
-            /*4*/if (firstFile.containsKey(secondFileEntry.getKey())
-                    && !firstFile.get(secondFileEntry.getKey()).equals(secondFileEntry.getValue())) {
-                mapOfKeysStatus.put(secondFileEntry.getKey(), KEY_STATUS[0]);//value by key is edited (key is present in both files but values are different)
-            }
-            /*2*/for (var firstFileEntry : firstMapEntrys) {
-                if (!secondFile.containsKey(firstFileEntry.getKey())) {
-                    mapOfKeysStatus.put(firstFileEntry.getKey(), KEY_STATUS[3]);//key removed (key from first file absent in second file)
-                }
-            }
-        }
-        /*for (var secondFileEntry : secondMapEntrys) {
             if (firstFile.containsKey(secondFileEntry.getKey())
-                    && firstFile.get(secondFileEntry.getKey()).equals(secondFileEntry.getValue())) {
+                    && Objects.equals(firstFile.get(secondFileEntry.getKey()), secondFileEntry.getValue())) {
                 mapOfKeysStatus.put(secondFileEntry.getKey(), KEY_STATUS[1]);
             }
             if (!firstFile.containsKey(secondFileEntry.getKey())) {
                 mapOfKeysStatus.put(secondFileEntry.getKey(), KEY_STATUS[0]);
             }
             if (firstFile.containsKey(secondFileEntry.getKey())
-                    && !firstFile.get(secondFileEntry.getKey()).equals(secondFileEntry.getValue())) {
+                    && !Objects.equals(firstFile.get(secondFileEntry.getKey()), secondFileEntry.getValue())) {
                 mapOfKeysStatus.put(secondFileEntry.getKey(), KEY_STATUS[0]);
             }
         }
@@ -50,7 +33,7 @@ public class Comparator {
             if (!secondFile.containsKey(firstFileEntry.getKey())) {
                 mapOfKeysStatus.put(firstFileEntry.getKey(), KEY_STATUS[0]);
             }
-        }*/
+        }
         return mapOfKeysStatus;
     }
 }
