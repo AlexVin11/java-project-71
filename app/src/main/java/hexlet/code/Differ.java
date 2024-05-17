cf5a211f0959d83302d4fa2de4e3e847280d74b5
Alexander Vinogradow <alukard981@gmail.com>
2024-05-17 22:33:00 +0300

    comparator class is created for generating diff between maps

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -7,7 +7,7 @@ import java.util.SortedMap;
 
 public class Differ {
 
-    static final String[] KEY_STATUS = {"key edited", "key not edited", "key added", "key removed"};
+    static final String[] KEY_STATUS = {"key edited", "key not edited"};
     static final Character[] EDIT_SIGN = {'+', '-'};
 
     public static String generate(String firstFile, String secondFile) throws Exception {
@@ -19,8 +19,8 @@ public class Differ {
         Path processedSecondPath = Path.of(secondFile).toAbsolutePath().normalize();
         String firstFileContent = Files.readString(peocessedFirstPath);
         String secondFileContent  = Files.readString(processedSecondPath);
-        Map<String, String> firstFileAsHashMap = Parser.parser(firstFileContent);
-        Map<String, String> secondFileAsHashMap = Parser.parser(secondFileContent);
+        Map<String, Object> firstFileAsHashMap = Parser.parser(firstFileContent);
+        Map<String, Object> secondFileAsHashMap = Parser.parser(secondFileContent);
         SortedMap<String, String> differenceMap = Comparator.generateKeyStatusHashMap(firstFileAsHashMap, secondFileAsHashMap);
 
         if (format.equals("stylish")) {
