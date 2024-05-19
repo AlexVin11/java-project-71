3db7e18986e980a6925e964e0ef7d789bc06adec
Alexander Vinogradow <alukard981@gmail.com>
2024-05-19 22:23:01 +0300

    checkstyle minor fix (1 line splitted to 2)

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -14,14 +14,17 @@ public class App implements Callable<Integer> {
 
     static final String HOME = System.getProperty("user.home");
 
-    @Option(names = {"-f", "--format"}, defaultValue = "stylish", description = "output format [default: stylish]", paramLabel = "format")
+    @Option(names = {"-f", "--format"}, defaultValue = "stylish",
+            description = "output format [default: stylish]", paramLabel = "format")
     String format;
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
     boolean helpRequested = false;
     @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
     boolean versionInfoRequested = false;
-    @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1") String firstFileName;
-    @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2") String secondFileName;
+    @Parameters(index = "0", description = "path to first file",
+            paramLabel = "filepath1") String firstFileName;
+    @Parameters(index = "1", description = "path to second file",
+            paramLabel = "filepath2") String secondFileName;
 
     public static void main(String[] args) {
         System.exit(new CommandLine(new App()).execute(args));
