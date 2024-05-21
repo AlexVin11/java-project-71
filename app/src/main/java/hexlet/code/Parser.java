b1924cd6552bcc9e884de4688ded3c6f34a79de4
Alexander Vinogradow <alukard981@gmail.com>
2024-05-21 17:55:54 +0300

    method for parsing map<String, Object> to map<String, String>

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -4,11 +4,12 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Parser {
 
-    public static Map<String, Object> parser(String content) throws Exception {
+    public static Map<String, Object> parseFileContentToMap(String content) throws Exception {
         if (content.startsWith("---")) {
             ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
             return mapper.readValue(content, new TypeReference<>() {
@@ -18,5 +19,14 @@ public class Parser {
             return mapper.readValue(content, new TypeReference<>() {
             });
         }
+    }
+
+    public static Map<String, String> convertMapValuesToString(Map<String, Object> fileContentAsMap) {
+        Map<String, String> resultMap = new HashMap<>();
+        var fileContentEntrySet = fileContentAsMap.entrySet();
+        for (var fileContentEntry : fileContentEntrySet) {
+            resultMap.put(fileContentEntry.getKey(), String.valueOf(fileContentEntry.getValue()));
+        }
+        return resultMap;
     }
 }
