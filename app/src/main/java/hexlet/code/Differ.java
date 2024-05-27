e3fbcf7b0890184c8711910eb735cc4bb301cd0f
Alexander Vinogradow <alukard981@gmail.com>
2024-05-27 22:46:58 +0300

    type of file variable added

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,5 +1,7 @@
 package hexlet.code;
 
+import org.apache.commons.io.FilenameUtils;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
@@ -12,12 +14,14 @@ public class Differ {
     }
 
     public static String generate(String firstFile, String secondFile, String format) throws Exception {
-        Path peocessedFirstPath = Path.of(firstFile).toAbsolutePath().normalize();
+        Path processedFirstPath = Path.of(firstFile).toAbsolutePath().normalize();
+        String firstFileType = FilenameUtils.getExtension(processedFirstPath.toString());
         Path processedSecondPath = Path.of(secondFile).toAbsolutePath().normalize();
-        String firstFileContent = Files.readString(peocessedFirstPath);
+        String secondFileType = FilenameUtils.getExtension(processedSecondPath.toString());
+        String firstFileContent = Files.readString(processedFirstPath);
         String secondFileContent  = Files.readString(processedSecondPath);
-        Map<String, Object> firstFileAsHashMap = Parser.parseFileContentToMap(firstFileContent);
-        Map<String, Object> secondFileAsHashMap = Parser.parseFileContentToMap(secondFileContent);
+        Map<String, Object> firstFileAsHashMap = Parser.parseFileContentToMap(firstFileContent, firstFileType);
+        Map<String, Object> secondFileAsHashMap = Parser.parseFileContentToMap(secondFileContent, secondFileType);
         SortedMap<String, String> differenceMap = Comparator.generateKeyStatusHashMap(firstFileAsHashMap,
                 secondFileAsHashMap);
         return Formatter.generateFormatedMessage(firstFileAsHashMap, secondFileAsHashMap, differenceMap, format);
