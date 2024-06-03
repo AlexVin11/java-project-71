2b9d4299432589a7c0b35af60b32b968f7a07caf
Alexander Vinogradow <alukard981@gmail.com>
2024-06-03 21:42:59 +0300

    Static variable added from valueAndState

--- app/src/main/java/hexlet/code/Comparator.java
+++ app/src/main/java/hexlet/code/Comparator.java
@@ -9,6 +9,11 @@ import java.util.TreeSet;
 
 public class Comparator {
 
+    public static final String EDITED = "edited";
+    public static final String NOT_EDITED = "not edited";
+    public static final String REMOVED = "removed";
+    public static final String ADDED = "added";
+
     public static Map<String, Object> generateKeyStatusHashMap(Map<String, Object> firstFile,
                                                                Map<String, Object> secondFile) {
         Set<String> keys = new TreeSet<>(firstFile.keySet());
@@ -18,22 +23,22 @@ public class Comparator {
         for (String key : keys) {
             if (firstFile.containsKey(key) && !secondFile.containsKey(key)) {
                 Object value = !Objects.isNull(firstFile.get(key)) ? firstFile.get(key) : null;
-                mapOfKeysStatus.put(key, new ValueAndState(ValueAndState.REMOVED,
+                mapOfKeysStatus.put(key, new ValueAndState(REMOVED,
                         value, null));
             }
             if (secondFile.containsKey(key) && !firstFile.containsKey(key)) {
                 Object value = !Objects.isNull(secondFile.get(key)) ? secondFile.get(key) : null;
-                mapOfKeysStatus.put(key, new ValueAndState(ValueAndState.ADDED,
+                mapOfKeysStatus.put(key, new ValueAndState(ADDED,
                         null, value));
             }
             if (firstFile.containsKey(key) && secondFile.containsKey(key)) {
                 Object firstValue = !Objects.isNull(firstFile.get(key)) ? firstFile.get(key) : null;
                 Object secondValue = !Objects.isNull(secondFile.get(key)) ? secondFile.get(key) : null;
                 if (Objects.equals(firstValue, secondValue)) {
-                    mapOfKeysStatus.put(key, new ValueAndState(ValueAndState.NOT_EDITED,
+                    mapOfKeysStatus.put(key, new ValueAndState(NOT_EDITED,
                             firstValue, secondValue));
                 } else {
-                    mapOfKeysStatus.put(key, new ValueAndState(ValueAndState.EDITED,
+                    mapOfKeysStatus.put(key, new ValueAndState(EDITED,
                             firstValue, secondValue));
                 }
             }
