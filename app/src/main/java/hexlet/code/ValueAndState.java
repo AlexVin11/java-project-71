54139b16f78dcf9709068b9898c3114b4a53b3a4
Alexander Vinogradow <alukard981@gmail.com>
2024-06-03 21:40:44 +0300

    Static variable moved to Comparator class where it is using

--- app/src/main/java/hexlet/code/ValueAndState.java
+++ app/src/main/java/hexlet/code/ValueAndState.java
@@ -9,11 +9,6 @@ import lombok.Setter;
 @AllArgsConstructor
 public class ValueAndState {
 
-    public static final String EDITED = "edited";
-    public static final String NOT_EDITED = "not edited";
-    public static final String REMOVED = "removed";
-    public static final String ADDED = "added";
-
     private String keyStatus;
     private Object oldValue;
     private Object newValue;
